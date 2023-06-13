d9b337b84a1f75941577974c2ddf3104cd3e9e1c
zulfuqar2 <anarzulfuqar2005@gmail.com>
2023-06-13 21:48:08 +0400

    about design

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -22,6 +22,33 @@ function About() {
   eum id sapiente aspernatur? Pariatur molestiae veritatis dicta
   laudantium harum, explicabo sit ex dolorem, amet, maxime nesciunt.
   </p>
+  <div className="skill">
+    <h1>My Dev Skills</h1>
+  <p>HTML</p>
+<div class="container">
+  <div class="skills html">90%</div>
+</div>
+
+<p>CSS</p>
+<div class="container">
+  <div class="skills css">80%</div>
+</div>
+
+<p>JavaScript</p>
+<div class="container">
+  <div class="skills js">55%</div>
+</div>
+
+<p>React</p>
+<div class="container">
+  <div class="skills react">40%</div>
+</div>
+<p>Redux</p>
+<div className="container">
+  <div className="skills redux"> 35%</div>
+  
+</div>
+  </div>
  
       </div>
     </div>
